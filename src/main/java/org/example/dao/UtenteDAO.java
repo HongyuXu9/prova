4015c8302f910ff7d2ae708c0bebd4b2306dca45
Hongyu Xu <38160604+HongyuXu9@users.noreply.github.com>
2025-01-13 23:47:14 +0100

    Delete src directory

--- src/main/java/org/example/dao/UtenteDAO.java
+++
@@ -1,7 +0,0 @@
-package org.example.dao;
-
-public interface UtenteDAO {
-    public boolean checkUtente(String username, String password);
-    public boolean checkRuolo(String username, String password);
-    public int registra(String username, String password, String ruolo);
-}
